7af77113f5d5502241115f06e5570df3615e912a
BranoGojkovic <branjevo1995@yandex.ru>
2021-08-15 18:00:43 +0200

    postownik -> post.svelte, readded authentication

--- postownik/app/client/src/App.svelte
+++ postownik/app/client/src/App.svelte
@@ -1,5 +1,7 @@
 <script>
-  import Postownik from './Panels/Postownik.svelte';
+  import Postownik from './Panels/Post.svelte';
+  import { authenticated } from './store.js';
+  $: console.log($authenticated);
 </script>
 
 <Postownik />
