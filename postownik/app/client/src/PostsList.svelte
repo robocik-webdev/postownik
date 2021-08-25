07aabe97a55672f5ab844b49578060eab63f016e
Hiro <pepapek@gmail.com>
2021-08-25 19:13:55 +0200

    Guess who's back

--- postownik/app/client/src/PostsList.svelte
+++ postownik/app/client/src/PostsList.svelte
@@ -72,8 +72,10 @@
 {/each} -->
 <div class="list">
     <button>Kalendarz</button>
+    <div class="filter">
     <h2>Filtrowanie</h2>
-    <select bind:value={selectedType}>
+    <label for="status">Status: </label>
+    <select bind:value={selectedType} name = "status">
       <option value="0">Wszystkie </option>
       <option value="1">Koncept Prywatny </option>
       <option value="2">Koncept</option>
@@ -82,6 +84,7 @@
       <option value="5">Gotowy</option>
       <option value="6"> Opublikowany</option>
     </select>
+  </div>
     <section class="slider">
         <section class="slider__posts">
             <!-- <PostOnList title = "tytuł" content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean convallis nisl id erat mattis maximus. Phasellus porta nibh sed dapibus finibus. Nam purus urna, hendrerit vel maximus ac, ultricies nec purus. Maecenas tempus lectus dui, ut pretium nunc viverra eget. Vivamus mollis dolor in enim dignissim imperdiet. Maecenas dui arcu, pulvinar vitae nunc eget, dignissim aliquam lacus. Quisque consectetur sollicitudin lacus ac euismod. Donec feugiat orci at elit semper, accumsan maximus massa consectetur. Donec ultrices risus justo, eget consectetur odio dapibus at. Curabitur purus odio, posuere quis ligula a, tincidunt faucibus ante. Sed dignissim est sit amet vehicula sollicitudin. Nullam sollicitudin quis neque in dictum." type = "private"/>
@@ -99,14 +102,17 @@
 
 <style>
 .slider__posts {
-  display: flex;
-  overflow-x: auto;
-  flex-wrap: nowrap;
-  flex-direction: row-reverse;
-  align-items: center;
+  display: grid;
+
+  /* overflow-x: auto; */
+  /* flex-wrap: nowrap;
+  flex-direction: column; */
+  /* align-items: center; */
+  /* grid-template-columns: repeat(2,40%); */
+  justify-content: center;
   padding-bottom: 30px;
+  margin: auto;
   margin-top: 30px;
-  width: auto;
   transition: transform 200ms;
 }
 
@@ -127,16 +133,36 @@ select{
   margin-bottom: 30px;
   font-size: 18px;
 }
+label{
+  font-size:22px;
+  margin-right: 10px;
+}
 @media (min-width: 600px) {
   select{
         width: 50%;
         font-size: 20px;
     }
+    
 }
 @media (min-width:1024px){
     select{
         width: 30%;
     }
+    
+.slider__posts{
+  grid-template-columns: repeat(2,50%);
+}
+}
+@media (min-width: 1248px){
+  div.filter{
+  position: sticky;
+  top: 20px;
+  width: 40%;
+}
+.slider__posts{
+  grid-template-columns: repeat(2,40%);
+  justify-content: end;
+}
 }
 
 
