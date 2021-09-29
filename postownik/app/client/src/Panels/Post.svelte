7f11f51d57f2599f417c7726373b09d62a914bb4
BranoGojkovic <branjevo1995@yandex.ru>
2021-09-29 22:32:39 +0200

    complete visual overhaul

--- postownik/app/client/src/Panels/Post.svelte
+++ postownik/app/client/src/Panels/Post.svelte
@@ -1,135 +1,272 @@
 <script>
-import { attr } from "svelte/internal";
-
+  import { attr } from 'svelte/internal';
 
   let status = [
-    { id: 'default', value: '-- zmień status postu --' },
-    { id: 'koncept', value: 'Koncept'},
-    { id: 'edycja', value: 'W edycji'},
-    { id: 'przeglad', value: 'Do przeglądu' },
-    { id: 'gotowy', value: 'Gotowy' }
+    { id: 'default', value: 'wybierz' },
+    { id: 'koncept', value: 'Koncept' },
+    { id: 'edycja', value: 'W edycji' },
+    { id: 'gotowy', value: 'Gotowy' },
   ];
 
   let selected = 'default';
-
- 
 </script>
 
 <div class="backgroundlayer">
   <div class="main">
-    <div class="content">
-      <h3>Postownik</h3>
+    <div class="header">
+      <img src="./rsc/logo.png" alt="logo" class="headerLogo" />
+      <h3>POSTOWNIK</h3>
     </div>
-    <div class="content">
-      <input class="title" type="text" placeholder="lorem" />
-    </div>
-    <div class="content">
-      <textarea
-        class="textarea"
-        wrap="hard"
-        rows="15"
-        cols="55"
-        placeholder="ipsum"
-      />
-    </div>
-    <div class="content">
-      <div class="drop-zone" />
-    </div>
-    <div class="content">
-      <div class="tag">
-        <input />
+    <div class="contentZone">
+      <div class="title">
+        <input type="text" class="inputTitle" placeholder="Tytuł" />
       </div>
-      <div class="status">
+      <hr />
+      <div class="textarea">
+        <textarea placeholder="Treść" />
+      </div>
+    </div>
+    <div class="attachmentZone">
+      <div class="poteznyPudzianPrzesuwaMojeDivy">
+        Załącznik &nbsp;&nbsp;&nbsp;
+        <input type="file" id="file" class="attachment" />
+        <label for="file" class="dummyAttachment">dodaj</label>
+        <div class="fileGallery">
+          <div class="file">plik ABC</div>
+        </div>
+      </div>
+    </div>
+    <div class="tagZone">
+      <div class="poteznyPudzianPrzesuwaMojeDivy">
+        Otaguj &nbsp;&nbsp;&nbsp;
+        <div class="tags"><input type="text" placeholder="wyszukaj" /></div>
+        <div class="tagGallery">
+          <div class="tag">tag 1</div>
+          <div class="tag">tag 2</div>
+          <div class="tag">tag 3</div>
+        </div>
+      </div>
+    </div>
+    <div class="status">
+      <div class="poteznyPudzianPrzesuwaMojeDivy">
+        Status &nbsp;&nbsp;&nbsp;
         <select class="statusList" bind:value={selected}>
           {#each status as status}
-            <option value={status.id}>{status.value}</option>>
+            <option value={status.id}>{status.value}</option>
           {/each}
         </select>
       </div>
     </div>
-    <div class="content">
-      <div class="buttonsetSmall">
-        <button class="btnSmall">Potencjalny post</button>
-        <button class="btnSmall">Post</button>
+  </div>
+  <div class="buttonZone">
+    <div class="post">
+      <div class="buttonContent">
+        <a href="https://www.youtube.com/watch?v=TJoeeTM2o50">Post</a>
       </div>
-      <div class="buttonsetSave">
-        <button class="btnSave">Zapisz</button>
+    </div>
+    <div class="spacer" />
+    <div class="save">
+      <div class="buttonContent">
+        <a href="https://www.youtube.com/watch?v=TJoeeTM2o50">Zapisz</a>
       </div>
     </div>
   </div>
 </div>
 
 <style>
-  option {
-    cursor: pointer;
-  }
-
-  option[disabled] {
-    background-color: var(--grey);
-  }
-
   .backgroundlayer {
-    margin: 10px 100px 10px 100px;
+    width: 600px;
+    height: 800px;
   }
 
   .main {
-    width: 600px;
-    color: var(--light);
+    width: 400px;
+    color: white;
+    margin: 0px 100px;
+    height: 700px;
   }
 
-  .content {
-    width: 600px;
-    cursor: default;
-    margin: 5px;
+  .poteznyPudzianPrzesuwaMojeDivy {
+    float: right;
+    margin: 10px 0 0;
   }
-  .title {
+
+  .header {
+    height: 26px;
+    width: 100%;
+    display: flex;
+    margin: 5px 0;
+  }
+
+  .headerLogo {
+    height: 100%;
+    display: inline-block;
+    width: 26px;
+  }
+
+  .header h3 {
+    color: #3b4a76;
+    margin-left: 5px;
+    padding: 0px;
+  }
+
+  .contentZone {
     position: static;
-    outline: none;
-    resize: none;
-    font-family: inherit;
-    width: 250px;
-    padding: 4px;
-    border: 1px solid var(--grey);
-    border-radius: 0;
-    cursor: inherit;
-    background-color: var(--light);
-    color: black;
-  }
-
-  .textarea {
-    position: static;
-    box-shadow: inherit;
-    line-height: 1.5;
-    outline: none;
-    resize: none;
-    width: 600px;
-    font-family: inherit;
-    background-color: var(--light);
-  }
-
-  .tag input {
-    width: 359px;
-  }
-
-  .tag,
-  .status {
-    display: inline;
-  }
-
-  .drop-zone {
-    height: 150px;
-    border: 2px dashed var(--grey);
+    width: inherit;
+    border: 2px solid #6c89bd;
     border-radius: 15px;
-    cursor: pointer;
-    margin-bottom: 10px;
   }
 
-  .content .buttonsetSmall {
+  .inputTitle {
+    background-color: white;
+    border: 0;
+    color: #3b4a76;
+    cursor: text;
+    margin: 0px 12px;
+    width: 370px;
+    font-size: 20px;
+  }
+
+  hr {
+    width: 350px;
+    margin: 0px 25px;
+    border: 1px solid #dce8fc;
+  }
+
+  textarea {
+    position: static;
+    line-height: 1.5;
+    padding: 8px;
+    outline: none;
+    resize: none;
+    font-family: inherit;
+    background-color: white;
+    color: var(--grey);
+    border: 0;
+    margin: 0px 15px;
+    width: 370px;
+    height: 400px;
+  }
+
+  .inputTitle::placeholder {
+    color: #3b4a76;
+  }
+
+  textarea::placeholder {
+    color: var(--grey);
+  }
+
+  .attachmentZone {
+    width: 100%;
+    color: #3b4a76;
+    position: inline-block;
+  }
+
+  .attachment {
+    display: none;
+  }
+
+  .dummyAttachment {
+    border: 2px solid #6c89bd;
+    padding: 2px 47px;
+    border-radius: 10px;
+    cursor: pointer;
+  }
+
+  .fileGallery {
+    margin: 10px 0 0;
+  }
+
+  .file {
+    color: #90b8fc;
+    border: 2px solid #c8dcfd;
+    border-radius: 10px;
+    padding: 2px 8px;
+    margin: 0 0 0 10px;
+    float: right;
+  }
+
+  .tagZone {
+    width: 100%;
+    color: #3b4a76;
+  }
+
+  .tags {
     display: inline;
   }
 
-  .btnSave {
-    width: 260px;
-    margin-top: 5px;
+  .tags input {
+    cursor: text;
+    width: 150px;
+    border-color: #6c89bd;
+    color: #3b4a76;
+  }
+
+  .tagGallery {
+    margin: 10px 0 0;
+  }
+
+  .tag {
+    color: #90b8fc;
+    border: 2px solid #c8dcfd;
+    display: inline-block;
+    border-radius: 10px;
+    padding: 2px 8px;
+    margin: 0 0 0 10px;
+  }
+
+  .status {
+    width: 100%;
+    color: #3b4a76;
+  }
+
+  option,
+  select {
+    color: #3b4a76;
+  }
+
+  select {
+    border-color: #6c89bd;
+    width: 150px;
+  }
+
+  .buttonZone {
+    width: 100%;
+    height: 35px;
+    display: inline-flex;
+    color: white;
+  }
+
+  .buttonZone a {
+    text-decoration: none;
+    color: white;
+    width: 250px !important;
+  }
+  .post {
+    width: 300px;
+    height: 100%;
+    background-color: #6c89bd;
+    border-radius: 0 10px 10px 0;
+  }
+
+  .buttonContent {
+    width: 100%;
+    padding:5px 10px;
+  }
+
+  .post .buttonContent{
+    margin-left:240px;
+  }
+  .spacer {
+    width: 50px;
+    height: 100%;
+    background-color: white;
+  }
+
+  .save {
+    width: 250px;
+    height: 100%;
+    background-color: #3b4a76;
+    border-radius: 10px 0 0 10px;
   }
 </style>
